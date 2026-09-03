6414a4db448706f0fc61ff21193cea32d919c2e9
Meraj Niaz Baloch <90909350+merajNizBaloch@users.noreply.github.com>
2026-09-04 01:52:47 +0500

    Load Contact page styles globally

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import "./products-home.css";
 import "./products-animations.css";
 import "./freebies/freebies.css";
 import "./freebies/freebie-factory.css";
+import "./contact/contact.css";
 import Navbar from "./components/navbar";
 import GlobalMouseEffect from "./components/global-mouse-effect";
 import GlobalScrollMotion from "./components/global-scroll-motion";
