d894a4964312be2c06e552c7c3d6435f971b7b5e
Meraj Niaz Baloch <90909350+merajNizBaloch@users.noreply.github.com>
2026-09-04 01:47:07 +0500

    Load Freebies styling globally

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import "./about/about-position.css";
 import "./products/products.css";
 import "./products-home.css";
 import "./products-animations.css";
+import "./freebies/freebies.css";
 import Navbar from "./components/navbar";
 import GlobalMouseEffect from "./components/global-mouse-effect";
 import GlobalScrollMotion from "./components/global-scroll-motion";
